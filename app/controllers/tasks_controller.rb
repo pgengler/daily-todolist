bf60e3153ba7922e3e9a516ad5694c36ef41c5d2
Phil Gengler <phil@pgengler.net>
2014-01-04 11:12:58 -0500

    Implement basic 'create' action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+	def create
+		Task.create! task_params
+		redirect_to new_task_path
+	end
+
+	private
+
+	def task_params
+		params.require(:task).permit(:summary)
+	end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -4,4 +4,10 @@ class TasksControllerTest < ActionController::TestCase
 	test "has a 'new' action" do
 		get :new
 	end
+
+	test "'create' action creates a new task" do
+		assert_difference 'Task.count' do
+			post :create, task: { summary: "A sample task" }
+		end
+	end
 end
